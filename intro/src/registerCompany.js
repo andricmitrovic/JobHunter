359fc827ec8b48ea47c4d948b6ff11d292aa2e50
Nikola Andric Mitrovic <andricmitrovicnikola@yahoo.com>
2021-04-14 11:25:41 +0200

    Added js code for adding more positions in registerCompany.

---
+++ intro/src/registerCompany.js
@@ -0,0 +1,49 @@
+var addPosition = function() {
+    console.log("YO")
+   };
+
+// var html = '<div>
+// <div class="form-group row justify-content-center">
+
+// <div class="col-2">
+//     <label for="jobField" class="control-label"> Job field: </label>
+//         <select name="jobField" id="jobField" class="form-control">
+//             <option value=""> ------ </option>
+//             <option value="si"> Software engineer </option>
+//             <option value="hr"> Human resources </option>
+//             <option value="mm"> Management </option>
+//             <option value="ac"> Accounting </option>
+//         </select>
+// </div>
+
+// <div class="col-2">
+//     <label for="explvl" class="control-label"> Experience level: </label>
+//     <select name="explvl" id="explvl" class="form-control">
+//         <option value=""> ------ </option>
+//         <option value="int"> Internship </option>
+//         <option value="jun"> Junior </option>
+//         <option value="sen"> Senior </option>
+//         <option value="con"> Contractor </option>
+//     </select>
+// </div>
+
+// <div class="form-group col-3">
+//     <label for="yearOfStudy">Required techonologies:</label>
+//     <select multiple class="form-control" id="yearOfStudy">
+//         <option>Python</option>
+//         <option>C++</option>
+//         <option>C#</option>
+//         <option>JS</option>
+//         <option>Java</option>
+//     </select>
+// </div>
+
+// <div class="form-group col-4">
+//     <label for="shortBio">Position description:</label>
+//     <textarea class="form-control" id="shortBio" rows="4"></textarea>
+// </div>
+// </div>
+// </div>';
+
+// var d1 = document.getElementById('position1');
+// d1.insertAdjacentHTML('afterend', html);
