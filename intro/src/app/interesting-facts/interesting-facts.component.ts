0caff2d0e425b8ba957b6e12869c6f3cf1166e0a
Tamara <tamarazdjukic@gmail.com>
2021-05-18 09:55:53 +0200

    added component for facts

---
+++ intro/src/app/interesting-facts/interesting-facts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interesting-facts',
+  templateUrl: './interesting-facts.component.html',
+  styleUrls: ['./interesting-facts.component.css']
+})
+export class InterestingFactsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
