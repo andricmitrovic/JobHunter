a50e5ba98cb2b441986247e402d738add501f5c3
Kristina <kpetrovic134@gmail.com>
2021-05-28 22:12:43 +0200

    Check student for user-profile

--- intro/src/app/pages/user-profile/user-profile.component.ts
+++ intro/src/app/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { StudentService } from './../../students/services/student.service';
+import { Student } from './../../students/models/student';
+import {  OnInit, Input, ViewChild, ElementRef, Component } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
+import jspdf from 'jspdf';
+import { FormBuilder } from '@angular/forms';
+import { JwtServiceCompanyService } from './../../../services/jwt-service-company.service';
+import { JwtService } from 'src/services/jwt.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +17,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  public student :Student;
+  @ViewChild('pdfTable') pdfTable: ElementRef;
+
+  constructor(private fb: FormBuilder, private studentService: StudentService, private JwtService: JwtService) {
+    // this.student = JwtService.getDataFromToken(); 
+    // TODOCHECK
   }
 
+
+  ngOnInit(): void {
+  
+  }
+
+  public SavePDF(): void {
+    const pdfTable = this.pdfTable.nativeElement;
+
+    const documentDefinition = {content: htmlToPdfmake(pdfTable.innerHTML)};
+
+    pdfMake.createPdf(documentDefinition).download('cv.pdf');
+  }
 }
