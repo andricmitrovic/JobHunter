15e68132f8e5d9a9f1fa51545dfebb340e58f6c1
Kristina <kpetrovic134@gmail.com>
2021-06-10 23:42:26 +0200

    Table view CV

--- intro/src/app/profile/students/students.component.ts
+++ intro/src/app/profile/students/students.component.ts
@@ -5,8 +5,9 @@ import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import htmlToPdfmake from 'html-to-pdfmake';
-import jspdf from 'jspdf';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 
 @Component({
@@ -23,21 +24,55 @@ export class StudentsComponent implements OnInit {
   @Input() student!: Student;
   @ViewChild('pdfTable') pdfTable: ElementRef;
 
+
+
+
+  head = [['ID', 'NAME', 'DESIGNATION', 'DEPARTMENT']]
+
+  data = [
+    [1, 'ROBERT', 'SOFTWARE DEVELOPER', 'ENGINEERING'],
+    [2, 'CRISTINAO','QA', 'TESTING'],
+    [3, 'KROOS','MANAGER', 'MANAGEMENT'],
+    [4, 'XYZ','DEVELOPER', 'DEVLOPEMENT'],
+    [5, 'ABC','CONSULTANT', 'HR'],
+    [73, 'QWE','VICE PRESIDENT', 'MANAGEMENT'],
+  ]
+
   constructor(private sS: StudentService, private modalService: NgbModal) {
   }
 
   ngOnInit(): void {
 
   }
-
+  
   public SavePDF(): void {
     
-    const pdfTable = this.pdfTable.nativeElement;
+    let doc = new jsPDF();
+    let templateType = 'templatedCV';
 
-    const documentDefinition = {content: htmlToPdfmake(pdfTable.innerHTML)};
+    doc.setFontSize(18);
+    doc.text(`CV ${this.student.personalInfo.fullName}`, 11, 8);
+    doc.setFontSize(11);
+    doc.setTextColor(156);
 
-    pdfMake.createPdf(documentDefinition).download('cv.pdf');
-  }
+
+    //  TODO popuniti tabelu podacima iz 
+    autoTable(doc, {
+      // columnStyles: { europe: { halign: 'center' } }, 
+      body: [
+        { PersonalInfo: 'Kristina', WorkExpirience: 'Google 9m', ContactInfo: 'kpetrovic134@gmail.com' },
+        { PersonalInfo: 'Marija', WorkExpirience: 'Canada', ContactInfo: 'mi15110@gmail.com' },
+      ],
+      columns: [
+        { header: 'PersonalInfo', dataKey: 'PersonalInfo' },
+        { header: 'WorkExpirience', dataKey: 'WorkExpirience' },
+        { header: 'ContactInfo', dataKey: 'ContactInfo' },
+      ]})
+
+    doc.output('dataurlnewwindow')
+    doc.save(`${this.student.personalInfo.fullName}_CV.pdf`);
+    }
+
 
     open(content) {
       this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
